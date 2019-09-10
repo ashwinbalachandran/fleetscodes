38363f960a58c363ae1ed83f2191fcb4ba9c13e9
ashwinbalachandran <ash20win@gmail.com>
2019-09-09 23:17:25 -0700

    Learning Kadane's algo

--- src/leets/KadanesAlgorithm.java
+++ src/leets/KadanesAlgorithm.java
@@ -8,6 +8,9 @@ import java.util.Scanner;
  * 1 2 3 -2 5
  * 4
  * -1 -2 -3 -4
+ * 1
+ * 11
+ * -2 -4 3 -1 5 6 -7 -2 4 -3 2
  */
 public class KadanesAlgorithm {
     
@@ -26,10 +29,21 @@ public class KadanesAlgorithm {
     }
 
     private static int getMaxSumSubArray(Integer[] nums) {
-        int maxSum = Integer.MIN_VALUE;
+        int maxSum = 0;
+        int s[] = new int[nums.length+1];
+        s[0] = 0;
+        int ctr = 1;
         for(Integer i:nums){
-            maxSum = Math.max(Math.max(i,maxSum),i+maxSum);
+           s[ctr] = s[ctr-1]+i;
+           ctr++;
         }
+        for(int i=1;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                int sax = s[j]-s[i-1];
+            }
+        }
+        for(int i=1;i<s.length;i++)
+        System.out.println(s[i]);
         return maxSum;
     }
 }
